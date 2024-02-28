69d2976dddda48a93d74e0996dc6559c1f9ea227
Murilo Cardoso dos Santos <cardoso.murilo2002@gmail.com>
2024-02-27 23:15:04 -0300

    IMC finished

--- imc-calc/js/index.js
+++ imc-calc/js/index.js
@@ -1,5 +1,7 @@
-function calcularIMC() {
-    const size = parseFloat(document.getElementById("size").value);
+function calcularIMC(event) {
+    event.preventDefault();
+
+    const size = parseFloat(document.getElementById("size").value) / 100;
     const weight = parseFloat(document.getElementById("weight").value);
     
     const calc = weight/(size*size);
@@ -9,15 +11,15 @@ function calcularIMC() {
     result.className = "result"
     
     if (calc < 17) {
-        result.textContent = `Seu IMC É DE ${calc} - VOCÊ ESTÁ MUITO ABAIXO DO PESO`
+        result.textContent = `Seu IMC É DE ${calc.toFixed(2)} - VOCÊ ESTÁ MUITO ABAIXO DO PESO`
     } else if (calc >= 17 && calc <= 18.49) { 
-        result.textContent = `Seu IMC É DE ${calc} - VOCÊ ESTA ABAIXO DO PESO`
+        result.textContent = `Seu IMC É DE ${calc.toFixed(2)} - VOCÊ ESTA ABAIXO DO PESO`
     } else if (calc >= 18.5 && calc <= 24.99) { 
-        result.textContent = `Seu IMC É DE ${calc} - VOCÊ ESTA NO PESO NORMAL`
+        result.textContent = `Seu IMC É DE ${calc.toFixed(2)} - VOCÊ ESTA NO PESO NORMAL`
     }else if (calc >= 25 && calc <= 29.99) { 
-        result.textContent = `Seu IMC É DE ${calc} - VOCÊ ESTA ACIMA DO PESO`
+        result.textContent = `Seu IMC É DE ${calc.toFixed(2)} - VOCÊ ESTA ACIMA DO PESO`
     }else if (calc >= 30 && calc <= 34.99) { 
-        result.textContent = `Seu IMC É DE ${calc} - OBESIDADE GRAU 1`
+        result.textContent = `Seu IMC É DE ${calc.toFixed(2)} - OBESIDADE GRAU 1`
     }
 }
 
